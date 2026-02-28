a3fd8f2b7d5fe93d89d140aa68fe0134bf768322
cryptanomale <168655035+cryptanomale@users.noreply.github.com>
2026-02-28 16:49:27 +0300

    Update mod.rs

--- crates/temporal-planetarium-lib/src/scans/trust_wallet/mod.rs
+++ crates/temporal-planetarium-lib/src/scans/trust_wallet/mod.rs
@@ -2,5 +2,8 @@ pub mod lcg;
 pub mod standard;
 
 pub use standard::run as run_standard;
-pub use lcg::run          as run_lcg;
-pub use lcg::run_multi_file as run_lcg_multi_file;
+pub use lcg::run              as run_lcg;
+pub use lcg::run_multi_file   as run_lcg_multi_file;
+
+#[cfg(feature = "gpu")]
+pub use lcg::run_bloom        as run_lcg_bloom;
